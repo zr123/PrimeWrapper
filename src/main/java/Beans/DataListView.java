0c8710e22082131ffa2df03fac46f5a43b94a039
zr123 <25320035+zr123@users.noreply.github.com>
2019-02-21 11:04:26 +0100

    Add files via upload

---
+++ src/main/java/Beans/DataListView.java
@@ -0,0 +1,47 @@
+package Beans;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
+@ViewScoped
+public class DataListView implements Serializable {
+
+    private List<Car> cars;
+
+    private Car selectedCar;
+    @PostConstruct
+    public void init() {
+        cars = new ArrayList<Car>();
+        cars.add(new Car("1", "BMW", 1990, "yellow", 3000, true));
+
+        /*
+        brands[1] = "Mercedes";
+        brands[2] = "Volvo";
+        brands[3] = "Audi";
+        brands[4] = "Renault";
+        brands[5] = "Fiat";
+        brands[6] = "Volkswagen";
+        brands[7] = "Honda";
+        brands[8] = "Jaguar";
+        brands[9] = "Ford";
+        */
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public Car getSelectedCar() {
+        return selectedCar;
+    }
+
+    public void setSelectedCar(Car selectedCar) {
+        this.selectedCar = selectedCar;
+    }
+}
+
